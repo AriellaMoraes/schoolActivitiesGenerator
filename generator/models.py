d230ed385fa6d0a638dcf682142a27b67f982f3a
Carlos Carvalho <juan.carvalho@prosperitainova.com.br>
2022-10-21 14:01:24 -0300

    Criação da classe Question, método save e load

---
+++ generator/models.py
@@ -0,0 +1,12 @@
+class Question:
+    def __init__(self) -> None:
+        print('Questão iniciada')
+
+    def save ():
+        '''Função que escreve em arquivo json'''
+        # with open(saida, 'w') as file:
+            # json.dump(resumo, file, indent=4)
+        pass
+
+    def load ():
+        pass
